2db56a57173d2167bfa97140e6aa5b927f91c946
Lucas Martins <lucasmariano.martins@outlook.com>
2020-12-27 00:26:31 -0300

    ⬆️ Update dependencies to run with python3.8

--- __init__.py
+++ __init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 import screen
 import screenshare
--- screen.py
+++ screen.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
-
-import threading, StringIO, time, base64
-import pyscreenshot as ig
+import threading, time, base64
+import pyscreenshot
+from io import BytesIO
 
 class Screen():
     def __init__(self):
-        self.FPS = 10
-        self.screenbuf = ""
-        self.screenfile = StringIO.StringIO()
+        self.FPS = 18
+        self.buffer = ""
+        self.screenfile = BytesIO()
         threading.Thread(target=self.getframes).start()
 
     def __del__(self):
@@ -15,13 +14,14 @@ class Screen():
 
     def getframes(self):
         while True:
-            im = ig.grab(childprocess=False)
-            self.screenfile.truncate(0)
+            im = pyscreenshot.grab(childprocess=False)
             im.save(self.screenfile, format="jpeg", quality=75, progressive=True)
-            self.screenbuf = base64.b64encode(self.screenfile.getvalue())
+            self.screenfile.seek(0)
+            im.close()
+            self.buffer = base64.b64encode(self.screenfile.getvalue())
             time.sleep(1.0/self.FPS)
     
     def gen(self):
-        return self.screenbuf
+        return self.buffer
     
 screenlive = Screen()
