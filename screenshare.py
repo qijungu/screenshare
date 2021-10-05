961b9f434f08607e6043d6d56d6abd70715f3492
Qijun Gu <qijun@txstate.edu>
2021-10-05 12:09:47 -0500

    add copyright and license

--- screenshare.py
+++ screenshare.py
@@ -1,5 +1,19 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#Copyright (C) 2021  Qijun Gu
+#
+#This file is part of Screenshare.
+#
+#Screenshare is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Screenshare is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Screenshare. If not, see <https://www.gnu.org/licenses/>.
 
 from flask import Flask, request, flash, session
 from flask_bootstrap import Bootstrap
